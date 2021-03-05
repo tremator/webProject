2bf9864dcca635bb2bbb364269fa715f016157bc
trombaman <trombaman@github.com>
2021-03-05 11:24:20 -0600

    changes old components

--- client-side-app/src/app/app-routing.module.ts
+++ client-side-app/src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BoardUserComponent } from './board-user/board-user.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
